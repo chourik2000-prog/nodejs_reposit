8c1b1b4a1e2c0f3855c7c10715e4ab8eec4b66b8
foussenichourik <foussenichourik@gmail.com>
2023-01-16 19:14:47 +0100

    first

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports ={
+    x:30,
+    y:20,
+    z:function(){
+        return 10;
+    }
+}
---
+++ data.js
@@ -0,0 +1,6 @@
+const app = require('./app')
+
+const arr=[2,4,7,1,3,8,5];
+arr.filter((item)=>{
+    console.log(item)
+}) 
